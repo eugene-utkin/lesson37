05494ad0dc6ea6a13d9e9420ac862f13e23c1db1
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-03 21:15:17 +0500

    Contacts Model Added and db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_124550) do
+ActiveRecord::Schema.define(version: 2020_09_03_160106) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
