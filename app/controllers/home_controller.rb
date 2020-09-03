8adfdc6cf3115788469a39d2d1993f2ebef25543
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-03 20:19:50 +0500

    Homework

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if params[:contact] != nil
+    render plain: params[:contact].inspect
+    end
+  end
+
+  def contacts
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/contacts'
+  post 'home/index'
 
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
