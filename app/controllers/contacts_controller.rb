9861e308c4cc1ce6decb2454362c6764e8208e31
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-03 20:59:19 +0500

    Contacts Controller and new.html.erb Added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,9 @@
 class ContactsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/contacts'
   post 'home/index'
 
+  resource :contacts, only: [:new, :create]
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
