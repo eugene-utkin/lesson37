23e041ae20e24a29c27a1ff27e41651e7af530d9
Evgeniy_Utkin <eugene.utkin@gmail.com>
2020-09-04 11:00:12 +0500

    contacts/new -> contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resource :contacts, only: [:new, :create]
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
